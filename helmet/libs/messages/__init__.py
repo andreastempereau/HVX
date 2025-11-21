9410525b4b875c19eca482c02c413df79721a4eb
andreastempereau <andreastempereau@gmail.com>
2025-11-20 17:36:46 -0800

    cleanup: remove microservices architecture (services/, gRPC clients, deployment files, cache)

--- helmet/libs/messages/__init__.py
+++
@@ -1,3 +0,0 @@
-# Helmet messages library
-from . import helmet_pb2
-from . import helmet_pb2_grpc
--- helmet/services/gps/__init__.py
+++
@@ -1,5 +0,0 @@
-"""GPS service for Ardusimple GPS tracking"""
-
-from .gps_service import GPSReader, GPSData
-
-__all__ = ['GPSReader', 'GPSData']
